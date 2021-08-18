ee22a5c938dab2691173028cc65fa85c3c48f575
nandinibaride <nandinibaride1234@gmail.com>
2021-08-18 15:52:37 +0530

    Added: Print Random Number Between 1 To 6

--- src/main/java/snakeandladderd4/com/snakeandladderd4.java
+++ src/main/java/snakeandladderd4/com/snakeandladderd4.java
@@ -2,7 +2,8 @@ package snakeandladderd4.com;
 public class snakeandladderd4 {
 public static void main(String[] args) {
 	System.out.println("Snake and Ladder game palyed with single player at start position 0");
-
+	double Rollingdie  = Math.floor(Math.random()*10) % 6+1;
+	System.out.println(Rollingdie);
 	
 	}	
 }
