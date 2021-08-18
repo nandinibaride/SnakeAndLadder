c1b75b4c2a17f4030e891f51fed34c4872f1b17b
nandinibaride <nandinibaride1234@gmail.com>
2021-08-18 15:46:53 +0530

    Added: Print Single Player At Start Position 0

--- src/main/java/snakeandladderd4/com/snakeandladderd4.java
+++ src/main/java/snakeandladderd4/com/snakeandladderd4.java
@@ -1,8 +1,8 @@
 package snakeandladderd4.com;
 public class snakeandladderd4 {
 public static void main(String[] args) {
-	
-	System.out.println("welcome");
+	System.out.println("Snake and Ladder game palyed with single player at start position 0");
+
 	
 	}	
 }
