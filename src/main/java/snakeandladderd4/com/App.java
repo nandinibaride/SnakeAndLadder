f3db725472db5c8710b8376429598a32e85362f7
nandinibaride <nandinibaride1234@gmail.com>
2021-08-18 15:39:03 +0530

    Added: welcome message

---
+++ src/main/java/snakeandladderd4/com/App.java
@@ -0,0 +1,13 @@
+package snakeandladderd4.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/snakeandladderd4/com/snakeandladderd4.java
@@ -0,0 +1,8 @@
+package snakeandladderd4.com;
+public class snakeandladderd4 {
+public static void main(String[] args) {
+	
+	System.out.println("welcome");
+	
+	}	
+}
