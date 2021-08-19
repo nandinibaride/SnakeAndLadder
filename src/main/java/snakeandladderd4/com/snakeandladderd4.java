105c27d6d8489400d769483c8f915b7d5226a177
nandinibaride <nandinibaride1234@gmail.com>
2021-08-19 22:38:21 +0530

    Adedd: Check option no play ladder or snake

--- src/main/java/snakeandladderd4/com/snakeandladderd4.java
+++ src/main/java/snakeandladderd4/com/snakeandladderd4.java
@@ -1,9 +1,25 @@
 package snakeandladderd4.com;
 public class snakeandladderd4 {
 public static void main(String[] args) {
-	System.out.println("Snake and Ladder game palyed with single player at start position 0");
-	double Rollingdie  = Math.floor(Math.random()*10) % 6+1;
-	System.out.println(Rollingdie);
 	
-	}	
+	System.out.println("Snake and Ladder game palyed with single player at start position 0");
+	int Rolling = (int)Math.floor(Math.random()*10) % 6+1;
+	System.out.println("Rolling :" + Rolling);
+		
+		int  position = 0;
+	    int check = (int) Math.floor(Math.random()*10)%3;
+        if (check == 0) {
+
+            System.out.println("no palyer ");
+        }
+        else if (check == 1) {
+            position += Rolling;
+            System.out.println(" ladder come ");
+        }
+        else {
+            position -= Rolling;
+            System.out.println("snake come ");
+        }
+    }
 }
+				
